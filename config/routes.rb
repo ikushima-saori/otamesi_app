8383b30019ea66a479f221bb7975ce20ccad0c21
ikushima-saori <172108271+ikushima-saori@users.noreply.github.com>
2024-08-06 07:45:53 +0000

    [Add]Idea/mvc

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get "homes/about"
 
   resources :users, only: [:index, :show, :edit]
+  resources :ideas, only: [:index, :show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
