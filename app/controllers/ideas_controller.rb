2022cbcb5992a6120f0b6223897009100ea330e4
ikushima-saori <172108271+ikushima-saori@users.noreply.github.com>
2024-08-07 08:24:26 +0000

    [Add]新規投稿機能&表示

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,10 +1,23 @@
 class IdeasController < ApplicationController
-  def index
+  def new
+    @idea = Idea.new
   end
 
   def show
   end
 
   def edit
+  end
+
+  def create
+    idea = Idea.new(idea_params)
+    idea.user_id = current_user.id
+    idea.save
+    redirect_to idea_path(idea.id)
+  end
+
+  private
+  def idea_params
+    params.require(:idea).permit(:category, :tag, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   get "homes/about"
 
   resources :users, only: [:index, :show, :edit, :update]
-  resources :ideas, only: [:index, :show, :edit]
+  resources :ideas, only: [:new, :show, :edit, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
